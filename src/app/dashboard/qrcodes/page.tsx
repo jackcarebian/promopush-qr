062000b82401d4627c4bf474a1376465fffc3094
jimmy tjahyono <jimmy.tjahyono@gmail.com>
2025-06-27 19:19:46 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/dashboard/qrcodes/page.tsx
+++ src/app/dashboard/qrcodes/page.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Check, QrCode, Store, ConciergeBell } from "lucide-react";
 
